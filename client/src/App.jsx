59de0515294b9bdc891a24fafd836bdda2d56921
Ken <legiacuong789@gmail.com>
2025-02-26 13:49:07 +0700

    Initialize Project

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import { Button } from "@material-tailwind/react";
+
+const App = () => {
+  return <Button>Button</Button>;
+};
+
+export default App;
---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <section></section>;
+};
+
+export default Navbar;
